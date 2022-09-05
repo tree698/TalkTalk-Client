b0c650936b798d935cc50b0f91eb81e31c953196
Chanwoo <tree698@gmail.com>
2022-09-05 12:06:28 +0900

    index: env+react-router+AuthProvider+authService(tokenStorage, httpclient)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthErrorEventBus, AuthProvider } from './context/authContext';
+import TokenStorage from './db/token';
+import HttpClient from './network/http';
+import AuthService from './service/auth';
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+const authErrorEventBus = new AuthErrorEventBus();
+const tokenStorage = new TokenStorage();
+const httpClient = new HttpClient(baseURL, authErrorEventBus);
+const authService = new AuthService(httpClient, tokenStorage);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider
+        authService={authService}
+        authErrorEventBus={authErrorEventBus}
+      >
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
