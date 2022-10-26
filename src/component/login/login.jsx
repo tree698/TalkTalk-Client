29e15b69e58933662c7f2140f95079e2315216a7
Chanwoo <tree698@gmail.com>
2022-10-26 13:04:46 +0900

    add CSS: login component

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -36,37 +36,55 @@ const LogIn = ({ onLogIn, onSignUpClick }) => {
   };
 
   return (
-    <div className={styles.login}>
-      <p className={styles.singup}>Don't you have an account?</p>
-      <button className={styles.signupBtn} onClick={onClick}>
-        SIGN UP
-      </button>
-      {text && <p>{text}</p>}
-      <h1 className={styles.title}>Welcome Back</h1>
-      <p className={styles.subTitle}>Login you account</p>
-      <form className={styles.login_form} onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={onChange}
-          className={styles.form_input}
-          placeholder="username"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChange}
-          className={styles.form_input}
-          placeholder="password"
-          required
-        />
-        <button className={styles.loginBtn} type="submit">
-          Login
-        </button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.login}>
+        <div className={styles.signup__container}>
+          <p className={styles.singupMessage}>Don't you have an account?</p>
+          <button className={styles.signupBtn} onClick={onClick}>
+            SIGN UP
+          </button>
+        </div>
+        {text && <p>{text}</p>}
+        <h1 className={styles.title}>Welcome Back</h1>
+        <p className={styles.subTitle}>Login your account</p>
+        <form className={styles.login__form} onSubmit={onSubmit}>
+          <div className={styles.input__container}>
+            <label for="username" className={styles.form__label}>
+              Username
+            </label>
+            <br />
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={onChange}
+              className={styles.form__input}
+              placeholder="Your username"
+              id="username"
+              required
+            />
+            <br />
+            <label for="password" className={styles.form__label}>
+              Password
+            </label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              className={styles.form__input}
+              placeholder="Your password"
+              id="password"
+              required
+            />
+            <br />
+          </div>
+          <button className={styles.loginBtn} type="submit">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
