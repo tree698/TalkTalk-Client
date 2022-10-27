4f0420bdfc2dcc4d73ef88db67b1b7032b69a198
Chanwoo <tree698@gmail.com>
2022-10-28 00:04:14 +0900

    add footer component

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <div className={styles.contact}>
+      <address>
+        <a className={styles.email} href="mailto:tree698@gmail.com">
+          tree698@gmail.com
+        </a>
+      </address>
+      <a
+        href="https://github.com/tree698"
+        className={styles.githubLinker}
+        target="_blank"
+      >
+        <FontAwesomeIcon icon={faGithub} className={styles.github} />
+      </a>
+    </div>
+    <p className={styles.right}>2023 Chanwoo @All Right Reserved</p>
+  </footer>
+);
+
+export default Footer;
