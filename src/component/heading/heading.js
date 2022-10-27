e72ed0d5c8708f6f5ec2dd3170bd76cba018dccd
Chanwoo <tree698@gmail.com>
2022-10-27 11:11:59 +0900

    modify CSS: heading + login

--- src/component/heading/heading.js
+++ src/component/heading/heading.js
@@ -4,14 +4,16 @@ import styles from './heading.module.css';
 
 const Heading = ({ workService }) => {
   return (
-    <div className={styles.heading}>
-      <h1 className={styles.title}>Talk & Talk</h1>
-      <h3 className={styles.description}>
-        Upload your artistics paintings. <br />
-        Then, enjoy talking with others.
-      </h3>
-      <div>
-        <Carousel workService={workService} />
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1 className={styles.title}>Talk & Talk</h1>
+        <h3 className={styles.description}>
+          Upload your artistics paintings. <br />
+          Then, enjoy talking with others.
+        </h3>
+        <div>
+          <Carousel workService={workService} />
+        </div>
       </div>
     </div>
   );
