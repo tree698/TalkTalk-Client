3b6b50967559e854f7136da4a12f860df1eae2ac
Chanwoo <tree698@gmail.com>
2022-10-27 16:56:53 +0900

    add fileUpload component
    	modified:   src/component/fileUpload/fileUpload.jsx
    	modified:   src/component/fileUpload/fileUpload.module.css

--- src/component/fileUpload/fileUpload.jsx
+++ src/component/fileUpload/fileUpload.jsx
@@ -1,24 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import styles from './fileUpload.module.css';
 
-const FileUpload = ({ workService, liftFile }) => {
+const FileUpload = ({ workService, liftFile, photo }) => {
+  const baseURL = process.env.REACT_APP_BASE_URL;
   const dropHandler = (file) => {
     workService.uploadImage(file).then(liftFile);
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       <Dropzone onDrop={dropHandler}>
         {({ getRootProps, getInputProps }) => (
-          <section>
+          <section className={styles.dropZone}>
             <div {...getRootProps()}>
               <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <button className={styles.message}>
+                Drag & drop <br /> or <br />
+                Click here
+              </button>
             </div>
           </section>
         )}
       </Dropzone>
+
+      <div className={styles.displayZone}>
+        {photo ? (
+          <img
+            src={`${baseURL}/uploaded_images/${photo}`}
+            className={styles.photo}
+          />
+        ) : (
+          <FontAwesomeIcon icon={faPlus} className={styles.icon} />
+        )}
+      </div>
     </div>
   );
 };
