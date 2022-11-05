cc3f7f7119824c99d1eb74ab245f6913c0e410ef
Chanwoo <tree698@gmail.com>
2022-11-05 20:11:51 +0900

    add loading spinner

--- src/component/fileUpload/fileUpload.jsx
+++ src/component/fileUpload/fileUpload.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
 import Dropzone from 'react-dropzone';
 import styles from './fileUpload.module.css';
 
 const FileUpload = ({ workService, liftFile }) => {
-  const dropHandler = (file) => {
-    workService.uploadImage(file).then(liftFile);
+  const [loading, setLoading] = useState(false);
+
+  const dropHandler = async (file) => {
+    setLoading(true);
+    const result = await workService.uploadImage(file);
+    setLoading(false);
+
+    liftFile(result);
   };
 
   return (
-    <Dropzone onDrop={dropHandler}>
-      {({ getRootProps, getInputProps }) => (
-        <section className={styles.container}>
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <button className={styles.message}>
-              Drag & Drop Here <br /> or Click Here
-            </button>
-          </div>
-        </section>
+    <>
+      <Dropzone onDrop={dropHandler}>
+        {({ getRootProps, getInputProps }) => (
+          <section className={styles.dropZone}>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <button className={styles.message}>
+                Drag & Drop Here <br /> or Click Here
+              </button>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+      {loading && (
+        <div className={styles.loading}>
+          <div className={styles.loading__animation}>Loading ...</div>
+        </div>
       )}
-    </Dropzone>
+    </>
   );
 };
 
