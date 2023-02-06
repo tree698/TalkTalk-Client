78135085d129555f7ed67619b4495c7476d51b65
Chanwoo <tree698@gmail.com>
2023-02-06 12:34:53 +0900

    add title + favicon + CSS for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,22 @@ import { BsGithub } from 'react-icons/bs';
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
-        <address>
-          <a href="mailto:tree698@gmail.com">tree698@gmail.com</a>
+    <footer className="flex flex-col items-center border-t-2 border-gray-300 p-4">
+      <div className="flex items-center mb-1">
+        <address className="mr-4">
+          <a className="hover:hover:text-brand" href="mailto:tree698@gmail.com">
+            tree698@gmail.com
+          </a>
         </address>
         <a
           href="https://github.com/tree698/TalkTalk-Client"
           target="_blank"
           rel="noreferrer"
         >
-          <BsGithub />
+          <BsGithub className="text-2xl hover:rotate-17 hover:text-brand hover:scale-110" />
         </a>
       </div>
-      <p>Thisis Chanwoo's portfolio.</p>
+      {/* <p>Thisis Chanwoo's portfolio.</p> */}
       <p>2023 Chanwoo @All Right Reserved</p>
     </footer>
   );
