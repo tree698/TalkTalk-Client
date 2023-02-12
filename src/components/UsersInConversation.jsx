71049a922bd5f21f84c20e11623e2dd468b42e79
Chanwoo <tree698@gmail.com>
2023-02-12 13:40:36 +0900

    add the functionality of real-time showing users who attend talk

--- src/components/UsersInConversation.jsx
+++ src/components/UsersInConversation.jsx
@@ -6,21 +6,35 @@ import { useQuery } from '@tanstack/react-query';
 import Avatar from './Avatar';
 
 export default function UsersInConversation({ onAvatarClick }) {
+  const STYLE_FOR_AVATAR = 'w-12 h-12 rounded-full mr-2';
+  const [length, setLength] = useState();
   const { tweetService } = useApiContext();
   const {
     state: { id },
   } = useLocation();
 
-  const { isLoading, error, data } = useQuery(['usersInConversation'], () =>
-    tweetService.getTweets('', id).then((data) => removeDuplicatedUser(data))
+  const { error, data } = useQuery(
+    ['usersInConversation'],
+    () =>
+      tweetService.getTweets('', id).then((data) => {
+        const result = removeDuplicatedUser(data);
+        setLength((prev) => result.length);
+        return result;
+      }),
+    {
+      enabled: !!'userInConversation',
+      refetchInterval: 2000,
+    }
   );
 
-  const STYLE_FOR_AVATAR = 'w-12 h-12 rounded-full mr-2';
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
 
   return (
     <section className="flex items-center px-3">
       <span className="w-12 h-12 text-center text-3xl border-2 border-darkGray p-1 rounded-full mr-4">
-        {calculateLength(data)}
+        {length}
       </span>
       <ul>
         {data &&
@@ -48,11 +62,5 @@ function removeDuplicatedUser(array) {
     photo.push(a.photo);
     result.push({ id: a.id, photo: a.photo, username: a.username });
   });
-  return result;
-}
-
-function calculateLength(data) {
-  let result = 0;
-  data.forEach((a) => (result += 1));
   return result;
 }
