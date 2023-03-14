fd9ea08220d1f1e9efeab89436f79df0200287f7
Chanwoo <tree698@gmail.com>
2023-03-14 14:35:51 +0900

    test code: ViewMore

---
+++ src/components/test/ViewMore.test.js
@@ -0,0 +1,34 @@
+import ViewMore from '../ViewMore';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('ViewMore', () => {
+  it('renders with button', () => {
+    const component = renderer.create(
+      <ViewMore lengthDrawings={3} limit={3} />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders without button', () => {
+    const component = renderer.create(
+      <ViewMore lengthDrawings={2} limit={3} />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('send client event', () => {
+    const mockOnClick = jest.fn();
+    render(
+      <ViewMore lengthDrawings={3} limit={3} onButtonClick={mockOnClick} />
+    );
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
+});
