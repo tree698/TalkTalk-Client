0bc7b5485841b0038033ab726fc4003734186142
Chanwoo <tree698@gmail.com>
2023-03-19 18:03:03 +0900

    test code: Upload page

---
+++ src/pages/test/Upload.test.js
@@ -0,0 +1,61 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Route } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { withApiContext, withRouter } from '../../test/utils';
+import Upload from '../Upload';
+import { FileUploadToCloudinary } from '../../uploader/FileUploadToCloudinary';
+
+jest.mock('../../components/Navbar');
+jest.mock('../../components/Footer');
+jest.mock('../../uploader/FileUploadToCloudinary');
+
+describe('Upload', () => {
+  const fakeContext = {
+    workService: {
+      createWork: jest.fn(),
+    },
+  };
+
+  afterEach(() => fakeContext.workService.createWork.mockReset());
+
+  it('renders correctly', () => {
+    const component = renderer.create(
+      withApiContext(
+        withRouter(<Route path="/" element={<Upload />} />),
+        fakeContext
+      )
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('display input value correctly', async () => {
+    render(
+      withApiContext(
+        withRouter(<Route path="/" element={<Upload />} />),
+        fakeContext
+      )
+    );
+
+    const inputElement = screen.getByRole('textbox', { name: /title/i });
+    userEvent.type(inputElement, 'test');
+
+    await waitFor(() => expect(inputElement).toHaveValue('test'));
+  });
+
+  // it('check correct url for image file', () => {
+  //   FileUploadToCloudinary.mockImplementation((url) => updateURL(url));
+  //   render(
+  //     withApiContext(
+  //       withRouter(<Route path="/" element={<Upload />} />),
+  //       fakeContext
+  //     )
+  //   );
+
+  //   userEvent.click(screen.getByTestId('fileUpload'));
+  //   const img = screen.getByTestId('targetImg');
+
+  //   expect(img.src).toBe('https://test.com');
+  // });
+});
